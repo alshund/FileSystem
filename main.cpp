d574e33888ab145b80e8c2422f869f240ee8614f
IvanYakimtsov <ivan.yakimtspv@gmail.com>
2017-11-21 09:16:33 +0300

    create file system class

--- main.cpp
+++ main.cpp
@@ -51,88 +51,99 @@ public:
     }
 };
 
-FileMapping *fileMapping = (FileMapping *) malloc(sizeof(FileMapping));
-DataBlock *fileSystemData;
 
-int init(size_t prefered_size) {
-    int blocksAmount = prefered_size / (DataBlock::block_size + DataBlock::pointer_size);
-    size_t size = blocksAmount *  (DataBlock::block_size + DataBlock::pointer_size);
+class FileSystem{
+public:
+    FileMapping *fileMapping = (FileMapping *) malloc(sizeof(FileMapping));
+    DataBlock *fileSystemData;
 
-    HANDLE hFile = CreateFile("file_system",
-                              GENERIC_READ | GENERIC_WRITE,
-                              0, //process access to the file = nothing
-                              nullptr,//security attribute
-                              CREATE_ALWAYS,
-                              FILE_ATTRIBUTE_NORMAL,
-                              nullptr);
-    if (hFile == INVALID_HANDLE_VALUE) {
-        return CREATE_FILE_ERROR;
+    int init(size_t prefered_size) {
+        int blocksAmount = prefered_size / (DataBlock::block_size + DataBlock::pointer_size);
+        size_t size = blocksAmount *  (DataBlock::block_size + DataBlock::pointer_size);
+
+        HANDLE hFile = CreateFile("file_system",
+                                  GENERIC_READ | GENERIC_WRITE,
+                                  0, //process access to the file = nothing
+                                  nullptr,//security attribute
+                                  CREATE_ALWAYS,
+                                  FILE_ATTRIBUTE_NORMAL,
+                                  nullptr);
+        if (hFile == INVALID_HANDLE_VALUE) {
+            return CREATE_FILE_ERROR;
+        }
+        char chunk[size];
+        memset(chunk, 0, size);
+        DWORD fileSize;
+        WriteFile(hFile, chunk, sizeof(chunk), &fileSize, nullptr);
+        DWORD dwFileSize = GetFileSize(hFile, nullptr); //nullptr - max file size = 4Gb
+        if (dwFileSize == INVALID_FILE_SIZE) {
+            CloseHandle(hFile);
+            return GET_FILE_SIZE_ERROR;
+        }
+
+        HANDLE hFileMapping = CreateFileMapping(hFile,
+                                                nullptr,
+                                                PAGE_READWRITE,
+                                                0,
+                                                size,
+                                                nullptr);
+        if (hFileMapping == nullptr) {
+            CloseHandle(hFile);
+            return CREATE_FILE_MAPPING_ERROR;
+        }
+
+        unsigned char *fileMappingPtr = (unsigned char *) MapViewOfFile(hFileMapping,
+                                                                        FILE_MAP_WRITE,
+                                                                        0,
+                                                                        0,
+                                                                        dwFileSize);
+        if (fileMappingPtr == nullptr) {
+            CloseHandle(hFileMapping);
+            CloseHandle(hFile);
+            return MAP_VIEW_OF_FILE_ERROR;
+        }
+
+
+        fileMapping->hFile = hFile;
+        fileMapping->hFileMapping = hFileMapping;
+        fileMapping->dataPtr = fileMappingPtr;
+        fileMapping->fileSize = (size_t) dwFileSize;
+
+        fileSystemData = new DataBlock[blocksAmount];
+
+        for (int i = 0; i < blocksAmount; i++) {
+            fileSystemData[i].ptr = fileMapping->dataPtr + i *  (DataBlock::block_size + DataBlock::pointer_size);
+        }
+        return 0;
     }
-    char chunk[size];
-    memset(chunk, 0, size);
-    DWORD fileSize;
-    WriteFile(hFile, chunk, sizeof(chunk), &fileSize, nullptr);
-    DWORD dwFileSize = GetFileSize(hFile, nullptr); //nullptr - max file size = 4Gb
-    if (dwFileSize == INVALID_FILE_SIZE) {
-        CloseHandle(hFile);
-        return GET_FILE_SIZE_ERROR;
+
+    ~FileSystem(){
+        UnmapViewOfFile(fileMapping->dataPtr);
+        CloseHandle(fileMapping->hFileMapping);
+        CloseHandle(fileMapping->hFile);
+        free(fileMapping);
     }
-
-    HANDLE hFileMapping = CreateFileMapping(hFile,
-                                            nullptr,
-                                            PAGE_READWRITE,
-                                            0,
-                                            size,
-                                            nullptr);
-    if (hFileMapping == nullptr) {
-        CloseHandle(hFile);
-        return CREATE_FILE_MAPPING_ERROR;
-    }
-
-    unsigned char *fileMappingPtr = (unsigned char *) MapViewOfFile(hFileMapping,
-                                                                    FILE_MAP_WRITE,
-                                                                    0,
-                                                                    0,
-                                                                    dwFileSize);
-    if (fileMappingPtr == nullptr) {
-        CloseHandle(hFileMapping);
-        CloseHandle(hFile);
-        return MAP_VIEW_OF_FILE_ERROR;
-    }
-
-
-    fileMapping->hFile = hFile;
-    fileMapping->hFileMapping = hFileMapping;
-    fileMapping->dataPtr = fileMappingPtr;
-    fileMapping->fileSize = (size_t) dwFileSize;
-
-    fileSystemData = new DataBlock[blocksAmount];
-
-    for (int i = 0; i < blocksAmount; i++) {
-        fileSystemData[i].ptr = fileMapping->dataPtr + i *  (DataBlock::block_size + DataBlock::pointer_size);
-    }
-}
+};
 
 int main() {
 
-    init(8000);
+    FileSystem *fileSystem = new FileSystem();
+
+    fileSystem -> init(8000);
 
     std::string temp2 = "dog 5";
 
 
-    fileSystemData[0].write(temp2.c_str());
+    fileSystem ->fileSystemData[0].write(temp2.c_str());
     std::string test;
-    test = fileSystemData[0].read();
+    test = fileSystem ->fileSystemData[0].read();
     std::cout << test<<"\n";
-    std::cout << fileSystemData[0].isEmpty()<<"\n";
-    std::cout << fileSystemData[5].isEmpty()<<"\n";
+    std::cout << fileSystem ->fileSystemData[0].isEmpty()<<"\n";
+    std::cout << fileSystem ->fileSystemData[5].isEmpty()<<"\n";
 
-    fileSystemData[0].setNext(666);
-    std::cout << fileSystemData[0].getNext();
+    fileSystem ->fileSystemData[0].setNext(666);
+    std::cout << fileSystem ->fileSystemData[0].getNext();
 
-    UnmapViewOfFile(fileMapping->dataPtr);
-    CloseHandle(fileMapping->hFileMapping);
-    CloseHandle(fileMapping->hFile);
-    free(fileMapping);
+    delete fileSystem;
     return 0;
 }
